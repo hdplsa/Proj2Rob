219cacd749276a600523e1f3af130ebea33b26a6
Hugo <hdplsa@gmail.com>
2016-04-27 21:04:22 +0100

    Adicionadas as funções do robô

--- init_robot.m
+++ init_robot.m
@@ -1,7 +1,11 @@
 function [sp] = init_robot(port)
     
-    sp = serial_port_start(port);
-    pioneer_init(sp);
+    if nargin == 0
+        port = 'COM4';
+    end
+    
+    sp = robot.serial_port_start(port);
+    robot.pioneer_init(sp);
     
 end
    
